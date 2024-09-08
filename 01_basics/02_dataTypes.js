b15a773e692eb0350bccea819e09b1bcf0d30eeb
Anurag Kumar <114359604+anuragak021@users.noreply.github.com>
2024-09-08 18:20:28 +0000

    new file

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all as JS as newer version 
+
+// alert(3+3)   another syntax in nodejs for this , we're using nodejs not browser console
+
+console.log(3+3);       //code readabilty should be there
+console.log("Anurag")
+
+let name = "Anurag"
+let islogegdIn = false
+let age = 18
+
+// number to the pow 53
+// big int
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);   //object
