3ead36181a93c39286e020e1e56e77e88a7ec8e4
Anurag Kumar <114359604+anuragak021@users.noreply.github.com>
2024-09-08 18:35:17 +0000

    new file on conversions

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,18 @@
+let score = "AN"
+
+console.log(typeof score);
+
+// as a method 
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// guarantee that it is converted to a number
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" = 33
+// "33abc"  NaN (Not a number , but type is a number);
+// true : 1 ; false : 0;
+
+let islogegdIn = 1;
