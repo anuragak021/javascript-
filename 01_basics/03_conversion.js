023163aad0c4d94e7edd6ea7b2fc3c12e85ba148
Anurag Kumar <114359604+anuragak021@users.noreply.github.com>
2024-09-09 18:47:22 +0000

    9th sept

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,18 +1,46 @@
 let score = "AN"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-// as a method 
-console.log(typeof(score));
+// // as a method 
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
 
 // guarantee that it is converted to a number
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" = 33
 // "33abc"  NaN (Not a number , but type is a number);
 // true : 1 ; false : 0;
 
 let islogegdIn = 1;
+
+/*****************************************************************operations*********/
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2*2);
+
+// let str1 = "hello"
+// let str2 = " Anurag"
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+
+let num1 , num2 , num3 
+
+num1 = num2 = num3 = 2 +2 
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
