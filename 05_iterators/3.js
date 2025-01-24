3edba0dd17744796ed4bf9643f6c523a081ebe9b
Anurag Kumar <114359604+anuragak021@users.noreply.github.com>
2025-01-24 18:29:53 +0000

    making changes in the iteratos

--- 05_iterators/3.js
+++ 05_iterators/3.js
@@ -17,23 +17,50 @@ for (const greet of greetings) {
 
 // Maps
 
-const map = new Map()
-map.set('IN' , "India")
-map.set('USA' , "United States of America")
-map.set('Fr' , "France")
-map.set('IN' , "India")
+// const map = new Map()
+// map.set('IN' , "India")
+// map.set('USA' , "United States of America")
+// map.set('Fr' , "France")
+// map.set('IN' , "India")
 
 // console.log(map);
 
-for (const [key , value] of map) {
-    // console.log(key , ':-' , value);
-}
+// for (const [key , value] of map) {
+//     // console.log(key , ':-' , value);
+// }
 
 const myObject = {
     'game1' : 'NFS' , 
     'game2' : 'SpiderMan'
 }
 
-for(const [key , value] of myObject){
-    console.log(key , ':-' , value);
+// for(const [key , value] of myObject){
+    // console.log(key , ':-' , value);
+// }
+
+const my = {
+    js: 'javascript',
+    cpp : 'C++',
+    rb : 'ruby' , 
+    swift : "swift by app"
 }
+
+for (const key in my) {
+    console.log(`${key} shortcut is for ${my[key]}`);
+}
+const programming = ["js" , "rb" , "py" , "java" , "cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN' , "India")
+// map.set('USA' , "United States of America")
+// map.set('Fr' , "France")
+// map.set('IN' , "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
